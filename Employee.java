eab548dbfc2da839b5f10f2fb2b45dc6aea33f76
RajaRamakhil <59257015+Svrap@users.noreply.github.com>
2019-12-31 16:53:59 +0530

    Jdbc

--- Employee.java
+++ Employee.java
@@ -1,65 +1,24 @@
-package dummy;
+package DBCON;
 
-//import java.util.*;
+import java.sql.Date;
 
-class Employee {
+public class Employee  {
 	int empid;
 	String ename;
 	double salary;
-
-
-
-
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + empid;
-		result = prime * result + ((ename == null) ? 0 : ename.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(salary);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Employee other = (Employee) obj;
-		if (empid != other.empid)
-			return false;
-		if (ename == null) {
-			if (other.ename != null)
-				return false;
-		} else if (!ename.equals(other.ename))
-			return false;
-		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "\nEmployee details\n [empid=" + empid + ", ename=" + ename + ", salary=" + salary + "]";
-	}
-
-	public Employee(int empid, String ename, double salary) {
+	Date doj;
+	public Employee(){};
+	public Employee(int empid, String ename, double salary, Date doj) {
+		super();
 		this.empid = empid;
 		this.ename = ename;
 		this.salary = salary;
+		this.doj = doj;
 	}
-
-	public  int getEmpid() {
+	public int getEmpid() {
 		return empid;
 	}
-	public  void setEmpid(int empid) {
+	public void setEmpid(int empid) {
 		this.empid = empid;
 	}
 	public String getEname() {
@@ -74,26 +33,11 @@ class Employee {
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
-}
-
-
-import java.util.TreeSet;
-
-public class EmpTree {
-
-	public static void main(String[] args)
-	{
-		TreeSet<Employee> tset =  new TreeSet<>();
-		tset.add(Employee(102,"babu",2000.00));
-		tset.add(Employee(101,"dj",5000.00));
-		tset.add(Employee(103,"vj",2000.00));
-		tset.add(Employee(104,"ajay",4000.00));
-		for(String s: tset)
-			System.out.println(s);
-
+	public Date getDoj() {
+		return doj;
+	}
+	public void setDoj(Date doj) {
+		this.doj = doj;
 	}
 }
-
-
-
-
+	
